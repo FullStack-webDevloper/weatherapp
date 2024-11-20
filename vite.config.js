897ae5f54c27cd0240e7b615b08ffa71d6a7ec8f
Altamash Ahmad <143337669+FullStack-webDevloper@users.noreply.github.com>
2024-11-20 19:39:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/weatherapp/",
+  base: '/weatherapp/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Assets folder
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // This generates hashed asset filenames
+      }
+    }
+  }
 })
+
